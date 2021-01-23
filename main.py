ab5efd16e11b94ad7ce97e9cc5d6037d86006536
AJ Kahle <ajkahle@gmail.com>
2021-01-22 19:16:54 -0600

    remove precincts reporting

--- main.py
+++ main.py
@@ -163,9 +163,10 @@ def test(event,context):
                 'candidate':r.find('div',class_='candidate-short-name').get_text(),
                 'party':r.find('div',class_='party-label').get_text(),
                 'votes':r.find('div',class_='candidate-votes-next-to-percent').get_text(),
-                'percent':r.find('div',class_='candidate-percent-only').get_text(),
-                'precincts':c.find('div','vote-progress').get_text(),
-                'called':c.find('div',class_='candidate-winner-check').get_text()
+                'percent':r.find('div',class_='candidate-percent-only').get_text()
+#                 ,
+#                 'precincts':c.find('div','vote-progress').get_text(),
+#                 'called':c.find('div',class_='candidate-winner-check').get_text()
             }
 
             print(_data)
